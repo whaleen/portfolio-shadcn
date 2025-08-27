ece94c2ee541b0dc778bd57839d93f5586f59562
Joshua Vaage <josh@Homes-MacBook-Pro.local>
2025-08-27 13:34:00 -0700

    any types

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,6 +3,15 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: any) {
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  // @ts-expect-error - next-themes type issue
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
